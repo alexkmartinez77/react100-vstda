14e78c5a117f64ad43388530bed6ff7e041f2050
Alex Martinez <alexkmartinez77@gmail.com>
2021-10-15 15:29:43 -0700

    Creates the basic structure of the app along with initializing state.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { 
+      toDoObj = {
+        priority = 1,
+        clicked = false,
+        textArea = '',
+      },
+      toDoArray = [],
+      }
+    }
 
   render() {
+
     return (
       <div className='container'>
-
+        <Header />
+        <div className="row">
+          <div className="col s-4">
+            <Form />
+          </div>
+          <div className="col s-8">
+            <List />
+          </div>
+        </div>
       </div>
     );
   }
