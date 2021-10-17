59df9a93b820450f0c2bd145c6e84bd6de4de412
Alex Martinez <alexkmartinez77@gmail.com>
2021-10-17 16:47:29 -0700

    Adds deleteObject which uses splice method to remove the chosen todo from the toDoArray.

--- src/ViewTodos.jsx
+++ src/ViewTodos.jsx
@@ -23,10 +23,10 @@ export class ViewTodos extends React.Component{
 
     return(
       <div className={bgColor}>
-        <input className="float-left" name="completed" type="checkbox" checked={completed} onChange={(e) => this.props.updateObject(e, i)}/>
+        <input className="float-left" type="checkbox" checked={completed} onChange={(e) => this.props.updateObject(e, i)}/>
         <span>{textArea}</span><span>{i}</span>
-        <a className="float-right">Edit</a>
-        <a className="float-right">Delete</a>
+        <a className="float-right ml-2" style={{color: 'blue', cursor: 'pointer'}} onClick={() => this.props.deleteObject(i)}>Delete</a>
+        <a className="float-right" style={{color: 'blue', cursor: 'pointer'}}>Edit</a>
       </div>
     )
   }
