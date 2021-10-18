7d0c48e94408ff34002602875a1451eeef7b126d
Alex Martinez <alexkmartinez77@gmail.com>
2021-10-18 14:11:53 -0700

    Merges 2 functions that do the same thing but to do different keys.

--- src/ViewTodos.jsx
+++ src/ViewTodos.jsx
@@ -47,7 +47,7 @@ export class ViewTodos extends React.Component{
             </div>
             <div className="row my-5 text-center">
               <div className="col-12">
-                <button className="btn btn-primary" name="submit" onClick={() => this.props.editSaveToggle(i)}>
+                <button className="btn btn-primary" name="submit" onClick={() => this.props.updateObject(i, 'edit')}>
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-calculator" viewBox="0 0 16 16">
                     <path d="M12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h8zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4z"/>
                     <path d="M4 2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5v-2zm0 4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm3-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm3-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-4z"/>
@@ -58,10 +58,10 @@ export class ViewTodos extends React.Component{
           </div>
           : 
           <div>
-            <input className="float-left" type="checkbox" checked={completed} onChange={() => this.props.updateObject(i)}/>
+            <input className="float-left" type="checkbox" checked={completed} onChange={() => this.props.updateObject(i, 'completed')}/>
             <span>{textArea}</span><span>{i}</span>
             <a className="delete-todo float-right ml-2" style={{color: 'blue', cursor: 'pointer'}} onClick={() => this.props.deleteObject(i)}>Delete</a>
-            <a className="edit-todo float-right" style={{color: 'blue', cursor: 'pointer'}} onClick={() => this.props.editSaveToggle(i)}>Edit</a>
+            <a className="edit-todo float-right" style={{color: 'blue', cursor: 'pointer'}} onClick={() => this.props.updateObject(i, 'edit')}>Edit</a>
           </div>
         }
       </div>
